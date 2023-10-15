9bb430e7c80217281a2245483235fe3ba066dcc8
sudo-vaibhav <mailvaibhavchopra@gmail.com>
2023-10-16 02:16:56 +0530

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# trybackroad
+# Try Backroad
+
+A simple starter for trying out backroad
+
+Use the following commands to get started:
+
+```bash
+git clone https://github.com/sudomakes/trybackroad
+cd trybackroad
+npm install
+npm run dev
+```
+
+## Links
+
+website: [backroad.sudomakes.art](https://backroad.sudomakes.art)
+github: [backroad](https://github.com/sudomakes/backroad)
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,5 @@
 import { run } from "@backroad/backroad";
 run(async (br) => {
-  const repoData = br.getOrDefault(
-    "repo-data",
-    null as null | { stars: number; latestVersion: string }
-  );
   br.write({
     body: `# 🛣️ Welcome to Backroad
 This is a quick start template to help you get started developing backroad apps. You can also checkout the examples on [stackblitz](https://stackblitz.com/@sudo-vaibhav/collections/backroad)`,
@@ -43,26 +39,4 @@ This is a quick start template to help you get started developing backroad apps.
 ---
 Backroad is currently in beta-development phase. If you like the idea behind it and would like to see it develop further. Please [consider starring Backroad on Github](https://github.com/sudomakes/backroad), or tell your developer friends about it. Thanks for trying backroad 💖`,
   });
-  if (repoData === null) {
-    const [repo, tags] = await Promise.all(
-      (
-        await Promise.all([
-          fetch("https://api.github.com/repos/sudomakes/backroad"),
-          fetch("https://api.github.com/repos/sudomakes/backroad/tags"),
-        ])
-      ).map((resp) => resp.json())
-    );
-
-    br.setValue("repo-data", {
-      stars: repo.stargazers_count,
-      latestVersion: tags[0].name,
-    });
-  } else {
-    br.stats({
-      items: [
-        { label: "Backroad Stars", value: repoData.stars },
-        { label: "Latest Version", value: repoData.latestVersion },
-      ],
-    });
-  }
 });
